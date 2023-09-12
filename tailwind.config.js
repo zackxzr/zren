d5411c027686c817c77e57d46de3c2e4115c94fa
Zack <renzackary@gmail.com>
2023-09-12 01:06:47 -0400

    import nextui, update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+import {nextui} from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./content/**/*.{md,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: ["class"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [nextui(), require("@tailwindcss/typography")],
 }
