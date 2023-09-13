393449595957ca245c83a117db67f213109e21a2
Zack <renzackary@gmail.com>
2023-09-12 21:09:12 -0400

    fix font colors on mode switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
-import Link from "next/link";
+"use client";
+
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme } = useTheme();
+
   return (
     <div className="mt-12">
-      <p className="text-zinc-300">
-        Hi, I&apos;m <span className="text-white">Zack</span>. I&apos;m a
-        Software Engineer.
+      <p className={`${theme === "light" ? "text-zinc-700" : "text-zinc-300"}`}>
+        Hi, I&apos;m{" "}
+        <span
+          className={`${
+            theme === "light" ? "text-black" : "text-white"
+          } font-semibold`}
+        >
+          Zack
+        </span>
+        . I&apos;m a Software Engineer.
       </p>
 
-      <p className="text-zinc-300 py-4">
+      <p
+        className={`${
+          theme === "light" ? "text-zinc-700" : "text-zinc-300"
+        } py-4`}
+      >
         On days when I decide I&apos;ve had enough of staring into a monitor, I
         enjoy going out for a hike or at least doing something active. I&apos;m
         always on the lookout for new things to learn and food to eat!
