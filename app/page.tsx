208d247ed7046bf0e6a2c7c65604f95c7aed146e
Zack <renzackary@gmail.com>
2023-09-12 21:59:34 -0400

    revert text colors to fix switching issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,20 @@
 "use client";
 
-import { useTheme } from "next-themes";
-
 export default function Home() {
-  const { theme } = useTheme();
-
   return (
     <div className="mt-12">
-      <p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"}`}>
-        Hi, I&apos;m{" "}
-        <span
-          className={`${
-            theme === "dark" ? "text-white" : "text-black"
-          } font-semibold`}
-        >
-          Zack
-        </span>
-        , a Software Engineer building out my ideas. Currently a SWE undergrad
-        student at McMaster University. I love solving challenging problems and
-        I&apos;m fascinated with innovations in AI and technology.
+      <p>
+        Hi, I&apos;m <span className="font-semibold">Zack</span>, a Software
+        Engineer building out my ideas. Currently a SWE undergrad student at
+        McMaster University. I love solving challenging problems and I&apos;m
+        fascinated with innovations in AI and technology.
       </p>
-      <p
-        className={`${
-          theme === "dark" ? "text-zinc-300" : "text-zinc-700"
-        } pt-4`}
-      >
+      <p className="pt-4">
         On days when I decide I&apos;ve had enough of staring into a monitor, I
         enjoy going out for a hike or at least doing something active. I&apos;m
         always on the lookout for new things to learn and food to eat!
       </p>
-      <p
-        className={`${
-          theme === "dark" ? "text-zinc-300" : "text-zinc-700"
-        } py-4`}
-      >
+      <p className="py-4">
         Right now I&apos;m building tools for web3 over at Parametric.
       </p>
     </div>
