48d2e6bbb541bd87f7fa2522f6860a140de4a071
Zack Ren <zren@Zacks-Mac.local>
2024-07-08 18:26:28 -0400

    update current activity on home

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <p>
         Hi, I&apos;m <span className="font-semibold">Zack</span>, a Software
         Engineer building out my ideas. I&apos;m a Software and Biomedical
-        Engineer Grad from McMaster University. I love solving challenging
+        Engineering graduate from McMaster University. I love solving challenging
         problems and I&apos;m fascinated with innovations in AI and technology.
       </p>
       <p className="pt-4">
@@ -14,9 +14,14 @@ export default function Home() {
         enjoy going out for a hike or at least doing something active. I&apos;m
         always on the lookout for new things to learn and food to eat!
       </p>
-      <p className="py-4">
-        Currently building the mobile app infrastructure at Capital One!
+      <p className="pt-4 pb-2">
+        What I'm up to:
       </p>
+      <ul className="list-disc pl-4">
+        <li>Software Engineering - Mobile backend and infrastructure <span className="font-medium">@Capital One</span></li>
+        <li>Master of Science in Computer Science - Machine Learning <span className="font-medium">@Georgia Tech</span></li>
+        <li>Technology I'm learning: Go, Bazel, AWS.</li>
+      </ul>
     </div>
   );
 }
