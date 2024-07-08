ece384b08ca03ea102d4cfa75a03e75ad2eca91d
Zack Ren <renzackary@gmail.com>
2024-07-08 18:31:31 -0400

    fix apos

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
         always on the lookout for new things to learn and food to eat!
       </p>
       <p className="pt-4 pb-2">
-        What I'm up to:
+        What I&apos;m up to:
       </p>
       <ul className="list-disc pl-4">
         <li>Software Engineering - Mobile backend and infrastructure <span className="font-medium">@Capital One</span></li>
         <li>Master of Science in Computer Science - Machine Learning <span className="font-medium">@Georgia Tech</span></li>
-        <li>Technology I'm learning: Go, Bazel, AWS.</li>
+        <li>Technology I&apos;m learning: Go, Bazel, AWS.</li>
       </ul>
     </div>
   );
